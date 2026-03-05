a5432f02b53112b7198224fd4997bbb78bbc878c
SlavKoVrn <slavko.chita@gmail.com>
2026-03-05 13:52:51 +0300

    swagger 5

--- public/index.php
+++ public/index.php
@@ -123,11 +123,11 @@ try {
         <html>
         <head>
             <title>API Docs</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css">
+            <link rel="stylesheet" href="/swagger5/swagger-ui.css">
         </head>
         <body>
             <div id="swagger-ui"></div>
-            <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+            <script src="/swagger5/swagger-ui-bundle.js"></script>
             <script>
                 SwaggerUIBundle({
                     url: "/swagger.json",
